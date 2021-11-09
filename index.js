c68e7c814c00d4f7ebc5f63a5e6d0e6e053e3f32
Iikka <t8kuii00@students.oamk.fi>
2021-11-09 20:05:04 +0200

    Exercise 3.8

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('data', (req, res) => {
+    if (req.method == "POST")
+        return JSON.stringify(req.body)
+    else
+        return
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+
 
 const persons = [
     {
